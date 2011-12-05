abb0a502650863eff6b5443da09af7e249ed5e3c
Arlen Avakian <arlen.prime@gmail.com>
2011-12-04 19:39:21 -0600

    gui: added main.

---
+++ src/gui/Kolourmatica.cpp
@@ -0,0 +1,32 @@
+/***********************************************************************
+|*  Copyright (C) 2010 Arlen Avakian
+|*
+|*  This file is part of Kolourmatica.
+|*
+|*  Kolourmatica is free software: you can redistribute it and/or modify
+|*  it under the terms of the GNU General Public License as published by
+|*  the Free Software Foundation, either version 3 of the License, or
+|*  (at your option) any later version.
+|*
+|*  Kolourmatica is distributed in the hope that it will be useful,
+|*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+|*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+|*  GNU General Public License for more details.
+|*
+|*  You should have received a copy of the GNU General Public License
+|*  along with Kolourmatica.  If not, see <http://www.gnu.org/licenses/>.
+|*
+|************************************************************************/
+
+
+#include "Console.hpp"
+#include <QtGui/QApplication>
+
+
+int main(int argc, char* argv[]){
+
+  QApplication app(argc, argv);
+  Console console;
+  console.show();
+  return app.exec();
+}
