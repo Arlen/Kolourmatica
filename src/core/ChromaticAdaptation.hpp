17062344b409833b09d5c15e1d65aa1ee44d4184
Arlen Avakian <arlen.prime@gmail.com>
2024-06-09 16:54:25 -0500

    core: refactored CA into separate classes.

--- src/core/ChromaticAdaptation.hpp
+++ src/core/ChromaticAdaptation.hpp
@@ -1,5 +1,5 @@
 /***********************************************************************
-|*  Copyright (C) 2010, 2011 Arlen Avakian
+|*  Copyright (C) 2010, 2011, 2024 Arlen Avakian
 |*
 |*  This file is part of Kolourmatica.
 |*
@@ -19,65 +19,85 @@
 |************************************************************************/
 
 
-#ifndef CHROMATIC_ADAPTATION_HPP
-#define CHROMATIC_ADAPTATION_HPP
+#pragma once
 
-#include "Illuminant.hpp"
+#include <Illuminant.hpp>
 
 #include <Eigen/Core>
-#include <Eigen/Dense>
-
-using namespace Eigen;
 
 
-template <class Real>
-struct AdaptationMethod{
- 
-  static const Matrix<Real, 3, 3> _XYZScaling;
-  static const Matrix<Real, 3, 3> _VonKries;
-  static const Matrix<Real, 3, 3> _Bradford;
-  static const Real xyzscaling[];
-  static const Real vonkries[];
-  static const Real bradford[];
-};
+namespace km::internal
+{
+    inline Eigen::Matrix3d
+    chromaticAdaptation(const Illuminant &src, const Illuminant &tar, const Eigen::Matrix3d &m)
+    {
+        using Eigen::Matrix3d;
 
-template <class Real>
-const Matrix<Real, 3, 3> AdaptationMethod<Real>::_XYZScaling =
-  Matrix<Real, 3, 3>(xyzscaling);
+        Point3d S = m * src.tri;
+        Point3d D = m * tar.tri;
+        Matrix3d tmp = Matrix3d::Zero();
+        tmp(0, 0) = D(0) / S(0);
+        tmp(1, 1) = D(1) / S(1);
+        tmp(2, 2) = D(2) / S(2);
 
-template <class Real>
-const Matrix<Real, 3, 3> AdaptationMethod<Real>::_VonKries =
-  Matrix<Real, 3, 3>(vonkries);
-
-template <class Real>
-const Matrix<Real, 3, 3> AdaptationMethod<Real>::_Bradford =
-  Matrix<Real, 3, 3>(bradford);
-
-template <class Real>
-const Real AdaptationMethod<Real>::xyzscaling[] =
-  {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0};
-
-template <class Real>
-const Real AdaptationMethod<Real>::vonkries[] =
-  {0.40024, -0.2263, 0.0, 0.7076, 1.16532, 0.0, -0.08081, 0.0457, 0.91822};
-
-template <class Real>
-const Real AdaptationMethod<Real>::bradford[] =
-  {0.8951, -0.7502, 0.0389, 0.2664, 1.7135, -0.0685, -0.1614, 0.0367, 1.0296};
-
-
-template <typename Real>
-Matrix<Real, 3, 3> chromaticAdaptationMatrix(const BaseIlluminant<Real>& source,
-					     const BaseIlluminant<Real>& target,
-					     const Matrix<Real, 3, 3>& method){
-
-  Matrix<Real, 3, 1> S = method * source.colour_XYZ().coords();
-  Matrix<Real, 3, 1> D = method * target.colour_XYZ().coords();
-  Matrix<Real, 3, 3> tmp = Matrix<Real, 3, 3>::Zero();
-  tmp(0, 0) = D(0) / S(0);
-  tmp(1, 1) = D(1) / S(1);
-  tmp(2, 2) = D(2) / S(2);
-  return method.inverse() * tmp * method;
+        return m.inverse() * tmp * m;
+    }
 }
 
-#endif
+
+namespace km
+{
+    struct ChromaticAdaptation
+    {
+        virtual ~ChromaticAdaptation() = default;
+        virtual Eigen::Matrix3d apply(const Illuminant& source, const Illuminant& target) = 0;
+    };
+
+    struct XYZScalingCA final : ChromaticAdaptation
+    {
+        Eigen::Matrix3d apply(const Illuminant& source, const Illuminant& target) override
+        {
+            return internal::chromaticAdaptation(source, target, Eigen::Matrix3d::Identity());
+        }
+    };
+
+    struct VonKriesCA final : ChromaticAdaptation
+    {
+        Eigen::Matrix3d apply(const Illuminant& source, const Illuminant& target) override
+        {
+            using Eigen::Matrix3d;
+            const Matrix3d method =
+                (Matrix3d() <<
+                    0.40024, -0.2263,
+                    0.0, 0.7076, 1.16532,
+                    0.0, -0.08081, 0.0457, 0.91822)
+                .finished();
+
+            return internal::chromaticAdaptation(source, target, method);
+        }
+    };
+
+    struct BradfordCA final : ChromaticAdaptation
+    {
+        Eigen::Matrix3d apply(const Illuminant& source, const Illuminant& target) override
+        {
+            using Eigen::Matrix3d;
+            const Matrix3d method =
+                (Matrix3d() <<
+                    0.8951, -0.7502, 0.0389,
+                    0.2664, 1.7135, -0.0685,
+                    -0.1614, 0.0367, 1.0296)
+                .finished();
+
+            return internal::chromaticAdaptation(source, target, method);
+        }
+    };
+
+    struct BypassCA final : ChromaticAdaptation
+    {
+        Eigen::Matrix3d apply(const Illuminant& source, const Illuminant& target) override
+        {
+            return Eigen::Matrix3d::Identity();
+        }
+    };
+}
